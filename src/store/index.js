2db663029fc9137124d7f8c00104a7de3e80369d
truong.nn <truong.nn@amanotes.com>
2023-09-05 09:42:20 +0700

    add redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-undef */
-import { combineReducers, createStore, compose } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 // reducer
-import { appReducer } from '../redux/app.reducer';
+import { appReducer } from "../redux/app.reducer";
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+  process.env.NODE_ENV === "development" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
 
 const rootReducer = combineReducers({
-  app: appReducer
-})
+  app: appReducer,
+});
 
-export const store = createStore(rootReducer, composeEnhancers());
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
